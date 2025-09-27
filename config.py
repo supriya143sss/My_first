19fa943065b741d6a718f301f7bbf83c4d358331
supriya143sss <supriyajaiswal0106@gmail.com>
2025-09-28 00:43:33 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+# Config file for Two-Factor Authentication Project
+
+# Your Gmail credentials (App Password required)
+SENDER_EMAIL = "kannaujiya.ruby446@gmail.com"
+EMAIL_PASSWORD = "aved lrtb lzxz kapb"
+
+# Flask secret key
+SECRET_KEY = "super_secret_key"
